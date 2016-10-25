c624192b30a1410d04e0d53a56d1463243d368bc
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-25 11:48:31 -0500

    update build.dat to copy release to ioLua

--- src/build.bat
+++ src/build.bat
@@ -2,3 +2,4 @@ del amalg.cache
 lua -lamalg PackageToolkit.lua
 lua amalg.lua -s PackageToolkit.lua -c -o ..\release\PackageToolkit.lua
 copy ..\release\PackageToolkit.lua ..\..\FunctionalX.lua\src
+copy ..\release\PackageToolkit.lua ..\..\ioLua\src
