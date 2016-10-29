5d78c40b3c76af5334e40ac18050e3a2f9fdf487
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-29 18:53:22 -0500

    update build.dat to copy files to the target's deps subfolder

--- src/build.bat
+++ src/build.bat
@@ -1,6 +1,7 @@
 del amalg.cache
 lua -lamalg PackageToolkit.lua
 lua amalg.lua -s PackageToolkit.lua -c -o ..\release\PackageToolkit.lua
-copy ..\release\PackageToolkit.lua ..\..\FunctionalX.lua\src
-copy ..\release\PackageToolkit.lua ..\..\ioLua\src
-copy ..\release\PackageToolkit.lua ..\..\umolflowFramework\src
+copy ..\release\PackageToolkit.lua ..\..\FunctionalX.lua\src\deps
+copy ..\release\PackageToolkit.lua ..\..\luapower
+copy ..\release\PackageToolkit.lua ..\..\ioLua\src\deps
+copy ..\release\PackageToolkit.lua ..\..\umolflowFramework\src\deps
