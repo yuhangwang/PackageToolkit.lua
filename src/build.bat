507845001941bf2cda83c3648b919b8d4968b716
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-29 17:21:16 -0500

    update build.bat to include umolflowFramework

--- src/build.bat
+++ src/build.bat
@@ -3,3 +3,4 @@ lua -lamalg PackageToolkit.lua
 lua amalg.lua -s PackageToolkit.lua -c -o ..\release\PackageToolkit.lua
 copy ..\release\PackageToolkit.lua ..\..\FunctionalX.lua\src
 copy ..\release\PackageToolkit.lua ..\..\ioLua\src
+copy ..\release\PackageToolkit.lua ..\..\umolflowFramework\src
